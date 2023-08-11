0d73fa02b70b62ae76efcb553016feff1ab51301
kacwojdev <wojnowski.kacper@gmail.com>
2023-08-11 17:41:50 +0200

    fix: remove unnecessary envs

--- src/firebase.js
+++ src/firebase.js
@@ -5,16 +5,18 @@ import { deleteDoc, getDocs, getFirestore, updateDoc, doc, collection } from 'fi
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 const db = getFirestore(app)
+
+console.log(firebaseConfig)
 
 const userDoc = () => doc(db, 'users', auth.currentUser.uid)
 const listDoc = listId => doc(db, 'users', auth.currentUser.uid, 'lists', listId)
