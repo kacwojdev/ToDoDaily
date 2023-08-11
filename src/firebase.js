c28cb067ac3c4f1f5fbbafaf18fa78d4ee435107
kacwojdev <wojnowski.kacper@gmail.com>
2023-08-11 14:00:53 +0200

    fix: change prcoess env to import meta env

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { deleteDoc, getDocs, getFirestore, updateDoc } from 'firebase/firestore'
 import { doc, collection } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: import.meta.env.REACT_APP_FIREBASE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
