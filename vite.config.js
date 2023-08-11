9b6095919bd8f2b4e705725b9ce910c6a6fd8bc2
kacwojdev <wojnowski.kacper@gmail.com>
2023-08-11 16:00:04 +0200

    fix: build path

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig(({ command, mode }) => {
         },
 
         build: {
-            outDir: 'dist'
+            outDir: './dist'
         },
         plugins: [react()],
 
